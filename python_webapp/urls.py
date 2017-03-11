9d9cf3ba9ac8c1c5cf7d3419533cae225665c21b
VReutov <ya.vreutov@yandex.ru>
2017-03-11 15:16:31 +0500

    Added: projects homepage

--- python_webapp/urls.py
+++ python_webapp/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^ontogen/', include('ontogen.urls')),
+    url(r'^\Z', include('home.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^ontogen/', include('ontogen.urls')),
 ]
