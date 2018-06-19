8dfa83f2011f416c415b561a9647272a40146cfa
Unknown <ya.vreutov@yandex.ru>
2018-06-20 03:26:43 +0500

    core constants

---
+++ ontogen/core/constants.py
@@ -0,0 +1,8 @@
+NO_NAME_NODE = 'o'
+NAME_RELATION = 'name'
+NUMBER_RELATION = 'number'
+IMAGE_RELATION = 'image'
+MODEL_RELATION = 'model'
+
+MATCHED_TEXT_ATTR = 'matched_text'
+MATCH_START_ATTR = 'match_start'
